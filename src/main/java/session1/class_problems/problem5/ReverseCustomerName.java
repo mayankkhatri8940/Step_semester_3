569f4a68f187ec3fe4af13a82da999ec6787d348
mayankkhatri8940 <mayankkhatri8940@gmail.com>
2026-09-06 16:27:03 +0530

    Add Session 1 class problem 5

---
+++ src/main/java/session1/class_problems/problem5/ReverseCustomerName.java
@@ -0,0 +1,26 @@
+package session1.class_problems.problem5;
+
+public class ReverseCustomerName {
+
+    public static String reverseCustomerName(String customerName) {
+
+        char[] characters = customerName.toCharArray();
+        String reversed = "";
+
+        for (int i = characters.length - 1; i >= 0; i--) {
+            reversed += characters[i];
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+
+        String customerName = "Sunil";
+
+        String reversedName = reverseCustomerName(customerName);
+
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Reversed Name: " + reversedName);
+    }
+}
