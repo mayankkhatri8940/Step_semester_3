e5b9b1afc83256cf903aa57a3d914b876245150a
mayankkhatri8940 <mayankkhatri8940@gmail.com>
2026-09-06 18:07:05 +0530

    Add Session 2 assignment problem 3

---
+++ src/main/java/session2/assignment_problems/problem3/ProductInventoryCSVParser.java
@@ -0,0 +1,26 @@
+package session2.assigment_problems.problem3;
+
+public class ProductInventoryCSVParser {
+
+    public static void parseInventoryRecord(String csvLine) {
+
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+            return;
+        }
+
+        System.out.println(
+                "Product: " + fields[0] +
+                        " | SKU: " + fields[1] +
+                        " | Qty: " + fields[2]
+        );
+    }
+
+    public static void main(String[] args) {
+
+        parseInventoryRecord("Wireless Mouse,WM-2201,150");
+        parseInventoryRecord("Wireless Mouse,150");
+    }
+}
